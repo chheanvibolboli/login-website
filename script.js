aa2ad03258656ad8a559f85599e21961722b929f
chheanvibolboli <160376898+chheanvibolboli@users.noreply.github.com>
2024-02-18 11:43:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // You can addlogic to check the credentials here
+    console.log('Usernaame: ' + username + ', Password: ' + password );
+}
